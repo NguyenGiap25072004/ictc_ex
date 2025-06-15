57d9aa7c2e93ed2d7692cc45731f2f79cd6add74
CSE for VNP <136301142+NguyenGiap25072004@users.noreply.github.com>
2025-06-16 00:36:59 +0700

    Update interrupt.v

--- interrupt.v
+++ interrupt.v
@@ -1,11 +1,13 @@
+// This module generates the final interrupt output signal
 module interrupt (
-	// Input from register block
-	input wire 	int_en,
-	input wire	int_st,
-	// System interrupt output
+	// Inputs from the register block
+	input wire 	int_en,		// Interrupt enable bit from TIER
+	input wire	int_st,		// Interrupt status bit from TISR
+	// Final interrupt signal to top-level
 	output wire	interrupt
 );
-	// Interrupt is asserted only when status is pending AND enable bit is set
-	assign interrupt = int_st & int_en;
 
+	// The interrupt signal is a logical AND of the status and enable bits.
+	assign interrupt = (int_st && int_en);
+	
 endmodule
