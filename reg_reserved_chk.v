2d6c1848381bbcd36a97c904dc4bca20216f2129
CSE for VNP <136301142+NguyenGiap25072004@users.noreply.github.com>
2025-06-16 10:41:53 +0700

    Update reg_reserved_chk.v

--- reg_reserved_chk.v
+++ reg_reserved_chk.v
@@ -1,12 +1,16 @@
 task run_test();
     reg [31:0] read_val;
-    reg [11:0] reserved_addr = 12'hF00;
+    reg [11:0] reserved_addr;
 begin
+    reserved_addr = 12'hF00; // Gán giá trị ở đây
+
     $display("\n\n--- Starting Test: Reserved Address Space ---");
-    $display("[SCENARIO] Writing and reading from a reserved address");
+    
+    $display("[SCENARIO] Writing and reading from a reserved address 0x%0h", reserved_addr);
     timer_reg_write(reserved_addr, 32'hFFFF_FFFF);
     timer_reg_read(reserved_addr, read_val);
     check_value(read_val, 32'h0, "Check reserved address follows RAZ/WI");
+
     if (g_error_count == 0) $display("--- TEST PASSED ---");
     else $display("--- TEST FAILED ---");
 end
